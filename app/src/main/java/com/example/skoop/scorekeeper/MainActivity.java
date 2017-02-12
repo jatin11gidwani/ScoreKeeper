0a11ffc17acd5136ad11be6b8b1146b635143135
Jatin Gidwani <jatin11gidwani@gmail.com>
2017-02-13 02:27:03 +0530

    Reset Button is rearranged to center in FootBall Game

--- app/src/main/java/com/example/skoop/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/skoop/scorekeeper/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.skoop.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,23 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     //BasketBall Team A Methods
     public void BB_Score_A_FreeThrow(View view) {
-        undo=BB_Score_A;
+        undo = BB_Score_A;
         BB_Score_A = BB_Score_A + 1;
         Show_BB_Score_A(BB_Score_A);
         flag = 1;
     }
 
     public void BB_Score_A_2Point(View view) {
-        undo=BB_Score_A;
+        undo = BB_Score_A;
         BB_Score_A = BB_Score_A + 2;
         flag = 1;
         Show_BB_Score_A(BB_Score_A);
     }
 
     public void BB_Score_A_3Point(View view) {
-        undo=BB_Score_A;
+        undo = BB_Score_A;
         flag = 1;
         BB_Score_A = BB_Score_A + 3;
         Show_BB_Score_A(BB_Score_A);
@@ -68,21 +67,21 @@ public class MainActivity extends AppCompatActivity {
 
     //BasketBall Team B Methods
     public void BB_Score_B_FreeThrow(View view) {
-        undo=BB_Score_B;
+        undo = BB_Score_B;
         BB_Score_B = BB_Score_B + 1;
         flag = 2;
         Show_BB_Score_B(BB_Score_B);
     }
 
     public void BB_Score_B_2Point(View view) {
-        undo=BB_Score_B;
+        undo = BB_Score_B;
         flag = 2;
         BB_Score_B = BB_Score_B + 2;
         Show_BB_Score_B(BB_Score_B);
     }
 
     public void BB_Score_B_3Point(View view) {
-        undo=BB_Score_B;
+        undo = BB_Score_B;
         BB_Score_B = BB_Score_B + 3;
         flag = 2;
         Show_BB_Score_B(BB_Score_B);
@@ -115,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         BB_Score_B = 0;
         BB_Foul_A = 0;
         BB_Foul_B = 0;
-        flag =5;
+        flag = 5;
         Show_BB_Score_A(BB_Score_A);
         Show_BB_Score_B(BB_Score_B);
         Show_BB_Foul_A(BB_Foul_A);
@@ -178,28 +177,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Bb_Undo(View View) {
-        if (flag == 1)
-        {   BB_Score_A = undo;
-            Show_BB_Score_A(undo);}
-        else if(flag == 2) {
+        if (flag == 1) {
+            BB_Score_A = undo;
+            Show_BB_Score_A(undo);
+        } else if (flag == 2) {
             BB_Score_B = undo;
             Show_BB_Score_B(undo);
-        }
-        else if(flag == 3) {
+        } else if (flag == 3) {
             BB_Foul_A = undo;
             Show_BB_Foul_A(undo);
-        }
-        else if(flag == 4)
-        {   BB_Foul_B = undo;
-            Show_BB_Foul_B(undo);}
-        else if (flag == 5)
-        {  Show_BB_Score_A(undor[0]);
+        } else if (flag == 4) {
+            BB_Foul_B = undo;
+            Show_BB_Foul_B(undo);
+        } else if (flag == 5) {
+            Show_BB_Score_A(undor[0]);
             Show_BB_Score_B(undor[1]);
             Show_BB_Foul_A(undor[2]);
             Show_BB_Foul_B(undor[3]);
             BB_Score_A = undor[0];
             BB_Score_B = undor[1];
             BB_Foul_A = undor[2];
-            BB_Foul_B = undor[3];}
+            BB_Foul_B = undor[3];
+        }
     }
 }
